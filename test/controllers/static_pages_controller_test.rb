7fa653bb27c363881dd130b5d1fee388b7190112
joelmwas <joelmwas199@gmail.com>
2015-04-24 01:12:33 +0300

    Added static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | CampusConnect"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", " Help | CampusConnect"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", " About | CampusConnect"
   end
 
 end
