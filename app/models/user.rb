b5f91807bf0ec1397f66352596671fd0f9d65765
joelmwas <joelmwas199@gmail.com>
2015-05-15 19:58:20 +0300

    user authentication done!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+	before_save { self.email = email.downcase }
+	validates :name, presence: true, lenght: { maximum: 50}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, lenght: { maximum: 255},
+					format: { with: VALID_EMAIL_REGEX }
+					uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, lenght: { minimum: 6 }
+
 	
 end
